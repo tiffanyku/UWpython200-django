2e8054acc7342c4bf714827ac5d1b5ad6efe3ada
Erica Winberry <elaewin@gmail.com>
2016-02-28 15:51:30 -0800

    added ability to publish multiple posts at once.

--- myblog/admin.py
+++ myblog/admin.py
@@ -7,13 +7,24 @@ class CategoryInline(admin.TabularInline):
 
 
 class PostAdmin(admin.ModelAdmin):
-    inlines = [
-        CategoryInline
-    ]
+    inlines = [CategoryInline]
+    list_display = ('title', 'author', 'created_date', 'modified_date')
+    actions = ['make_published']
+
+    def make_published(self, request, queryset):
+        rows_updated = queryset.update(status='p')
+        if rows_updated == 1:
+            message_bit = "1 post was"
+        else:
+            message_bit = "{} posts were".format(rows_updated)
+        self.message_user(request, "{} successfully marked as published.".format(message_bit))
+    make_published.short_description = "Mark selected posts as published."
 
 
 class CategoryAdmin(admin.ModelAdmin):
     fields = ('name', 'description')
 
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category, CategoryAdmin)
+
